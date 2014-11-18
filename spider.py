e546f3429f0c10b4f734c00608e1bd663e954e75
huguangju <huguangjugit@163.com>
2014-11-19 00:17:05 +0800

    reconstruction this app with bs4

--- spider.py
+++ spider.py
@@ -2,10 +2,10 @@
 
 import urllib2
 import urllib
-import re
 import os
 import errno
 import urlparse
+from bs4 import BeautifulSoup
 
 
 class Spider:
@@ -15,31 +15,24 @@ class Spider:
         self.base_url = 'http://webapplayers.com/inspinia_admin-v1.5/'
         self.save_path = 'downloads/'
 
-    def traversal_links(self, url):
-        response = urllib2.urlopen(url)
-        content = response.read().decode("utf-8")
-        links = []
+    def getContent(self, url):
+        response = urllib2.urlopen('http://webapplayers.com/inspinia_admin-v1.5/')
+        return response.read().decode('utf-8')
 
-        # css
-        p = re.compile(r'<link.+?href=.+?>')
-        pname = re.compile( r'(?<=>)')
-        phref = re.compile( r'(?<=href\=\").*?(?=\")')
+    def getUrls(self, content):
+        soup = BeautifulSoup(''.join(content))
+        result = []
 
-        # TODO:javascript
-        # TODO:images
-        # TODO:fonts
+        #images
+        links = soup.findAll('img')
+        for link in links:
+            try:
+                temp = link['src']
+                result.append(temp)
+            except KeyError:
+                pass # or some other fallback action
 
-        items = p.findall(content)
-        for item in items:
-            sname = pname.findall(item)
-            if sname:
-                sname = sname[0]
-                shref = phref.findall(item)
-            if shref:
-                shref = shref[0]
-
-            links.append(shref)
-        return links
+        return result
 
     def getdirc(self, url):
         idx = url.rfind('/')
@@ -48,51 +41,46 @@ class Spider:
     # 下载到本地
     def download(self, url, save_path):
         # TODO:是否基路径就是首页
-
-        # 除去文件名后的相对路径
-        #if url != self.base_url:
-        #    dir = self.getdirc(url)
-
-        # 是否是下载的首页
-        is_index_page = True
-
         if len(save_path) == 0:
             save_path = self.save_path
 
-        if is_index_page:
-            if not os.path.exists(self.save_path):
-                self.mkdir_p(self.save_path)
+        if not os.path.exists(save_path):
+            self.mkdir_p(save_path)
 
-            # 下载主页
-            if not os.path.isfile(self.save_path + self.index_filename):
-                urllib.urlretrieve(url, self.save_path + self.index_filename)
-                print 'index page save as:' + self.index_filename
+        absolute_url = urlparse.urljoin(self.base_url, url)
+        dirs = self.getdirc(save_path + url)
+        if not os.path.exists(dirs):
+            self.mkdir_p(dirs)
+            print 'create directory:' + dirs
 
-            # 下载其它资源文件
-            links = self.traversal_links(url)
-            for link in links:
-                absolute_url = urlparse.urljoin(self.base_url, link)
-                dirs = self.getdirc(save_path + link)
-                if not os.path.exists(dirs):
-                    self.mkdir_p(dirs)
-                    print 'create directory:' + dirs
-
-                    urllib.urlretrieve(absolute_url, save_path + link)
-                    print 'download file:' + absolute_url
+        if not os.path.exists(save_path + url):
+            urllib.urlretrieve(absolute_url, save_path + url)
+            print 'download file:' + absolute_url
 
     # 递归创建文件夹
     def mkdir_p(self, path):
         try:
-            os.makedirs(path)
+            if not path.__contains__('.'):
+                os.makedirs(path)
         except OSError as exc: # Python >2.5 (except OSError, exc: for Python <2.5)
             if exc.errno == errno.EEXIST and os.path.isdir(path):
                 pass
             else:
                 raise
 
+    def isAbsolute(self, link):
+        for item in ['http://', 'https://']:
+            try:
+                if link.index(item) >= 0:
+                    return True
+            except:
+                pass
+        return False
+
 if __name__ == "__main__":
-    myModel = Spider()
-    #url = raw_input('type url, please : ')
-    #filename = raw_input('type main file name : ')
-    myModel.download('http://webapplayers.com/inspinia_admin-v1.5/','')
+    spider = Spider()
+    content = spider.getContent('http://webapplayers.com/inspinia_admin-v1.5/');
+    for href in {}.fromkeys(spider.getUrls(content)).keys():
+        spider.download(href, 'temp/')
+
 
