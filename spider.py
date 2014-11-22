d56a7b6a4caf39cb817f93f4fbb9c521811c222a
huguangju <huguangjugit@163.com>
2014-11-23 02:07:27 +0800

    download images from css file

--- spider.py
+++ spider.py
@@ -8,6 +8,7 @@ import urlparse
 from bs4 import BeautifulSoup
 import socket
 import time
+import re
 
 class Spider:
 
@@ -15,28 +16,49 @@ class Spider:
         self.base_url = ''
         self.save_path = 'downloads/'
         timeout = 25
-		
-		# 这里对整个socket层设置超时时间。后续文件中如果再使用到socket，不必再设置
-        socket.setdefaulttimeout(timeout) 
+
+        # 这里对整个socket层设置超时时间。后续文件中如果再使用到socket，不必再设置
+        socket.setdefaulttimeout(timeout)
         self.downloadSuccess = False
         self.index_page = 'index.html'
 
-    def getContent(self, url):
+    def getContent(self, url, save_path):
         self.base_url = url
-		
+
         # TODO:如果根路径就是首页则下载
-        urllib.urlretrieve(url, self.save_path + self.index_page, self.download_callbackfunc)
+        if not os.path.exists(save_path):
+            self.mkdir_p(save_path)
+
+        urllib.urlretrieve(url, save_path + self.index_page, self.download_callbackfunc)
 
         response = urllib2.urlopen(url)
         content = response.read().decode('utf-8')
         response.close()
         return content
 
+    def get_img_in_css(self, url):
+        request = urllib2.Request(url)
+        html = urllib2.urlopen(request).read().decode('utf-8')
+
+        # background: url("../images/arrow.png") no-repeat scroll right 14px top 18px rgba(0, 0, 0, 0);
+        re_string = r'background.*url\("(.*)"\)'
+        match = re.compile(re_string)
+        img_links = re.findall(match, html)
+        img_result = []
+
+        for img in img_links:
+            # TODO:只有一层相对目录的情况，考虑多层或无限层解析相对目录
+            if img.count('../') == 1:
+                img_result.append(url[:url.rfind('/')+1] + img.replace('../', ''))
+                #print img, img.count('../'), url.rfind('/'), url[:url.rfind('/')]
+
+        return img_result
+
     def getUrls(self, content):
         soup = BeautifulSoup(''.join(content))
         result = []
 
-         #a
+        #a
         links = soup.findAll('a')
         for link in links:
             try:
@@ -78,12 +100,12 @@ class Spider:
         idx = url.rfind('/')
         return url[:idx]
 
+    '''下载文件的回调函数
+    @blocknum: 已经下载的数据块
+    @blocksize: 数据块的大小
+    @totalsize: 远程文件的大小
+    '''
     def download_callbackfunc(self,blocknum, blocksize, totalsize):
-        '''回调函数
-        @blocknum: 已经下载的数据块
-        @blocksize: 数据块的大小
-        @totalsize: 远程文件的大小
-        '''
         percent = 100.0 * blocknum * blocksize / totalsize
         if percent > 100:
             percent = 100
@@ -102,6 +124,7 @@ class Spider:
 
         absolute_url = urlparse.urljoin(self.base_url, url)
         dirs = self.getdirc(save_path + url)
+
         if not os.path.exists(dirs):
             self.mkdir_p(dirs)
             print 'create directory:' + dirs
@@ -109,25 +132,27 @@ class Spider:
         if not os.path.exists(save_path + url):
             sleep_time = 1
             while True:
-                try: # 外包一层try
+                try:
                     time.sleep(sleep_time)
+
+                    print 'downloading file:' + absolute_url
                     urllib.urlretrieve(absolute_url, save_path + url, self.download_callbackfunc)
-                    print 'download file:' + absolute_url
-                   # 只有正常执行方能退出。
+
+                    # 只有下载成功方能退出
                     if self.downloadSuccess:
                         break
-                except : # 如果发生了10054或者IOError或者XXXError
-                    sleep_time += 5 #多睡5秒，重新执行以上的download.因为做了检查点的缘故，上面的程序会从抛出异常的地方继续执行。防止了因为网络连接不稳定带来的程序中断。
-                    print('enlarge sleep time:',sleep_time)
 
-
-
+                # 如果发生了10054或者IOError或者XXXError
+                except:
+                    # 多睡5秒，重新执行以上的download.因为做了检查点的缘故，上面的程序会从抛出异常的地方继续执行。
+                    # 防止了因为网络连接不稳定带来的程序中断
+                    sleep_time += 5
+                    print('enlarge sleep time:', sleep_time)
 
     # 递归创建文件夹
     def mkdir_p(self, path):
         try:
-            if not path.__contains__('.'):
-                os.makedirs(path)
+            os.makedirs(path)
         except OSError as exc: # Python >2.5 (except OSError, exc: for Python <2.5)
             if exc.errno == errno.EEXIST and os.path.isdir(path):
                 pass
@@ -145,10 +170,21 @@ class Spider:
 
 if __name__ == "__main__":
     spider = Spider()
-    content = spider.getContent('http://webapplayers.com/inspinia_admin-v1.6/Angular/#/dashboards/dashboard_1');
-    spider.save_path = "test/"
+    base_url = 'http://foxythemes.net/cleanzone2/'
+    save_path = "test/"
+    #content = spider.getContent(base_url, "test/");
     for href in {}.fromkeys(spider.getUrls(content)).keys():
-        if not href.__contains__('https://maps.googleapis.com/maps/api'):
-            spider.download(href, spider.save_path)
+        if not href.__contains__('googleapis'):
+            spider.download(href, save_path)
 
+     # 单独下载css中的背景图
+    '''imgs = spider.get_img_in_css('http://foxythemes.net/cleanzone2/css/style.css')
+    for img in imgs:
+        temp = img.replace(base_url, '')
+        dirs = save_path + temp[:temp.rfind('/')]
+        if not os.path.exists(dirs):
+            spider.mkdir_p(dirs)
+
+        urllib.urlretrieve(img, save_path + temp)
+        print 'downloading img: ', img'''
 
