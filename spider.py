21264c7919511fde32ac55d16e8aecb24349f28f
huguangju <huguangjugit@163.com>
2014-11-22 17:34:02 +0800

    resolve error: IOError: [Errno socket error] [Errno 10053]

--- spider.py
+++ spider.py
@@ -6,22 +6,62 @@ import os
 import errno
 import urlparse
 from bs4 import BeautifulSoup
-
+import socket
+import time
 
 class Spider:
 
     def __init__(self):
-        self.index_filename = 'index.html'
-        self.base_url = 'http://webapplayers.com/inspinia_admin-v1.5/'
+        self.base_url = ''
         self.save_path = 'downloads/'
+        timeout = 25
+		
+		# 这里对整个socket层设置超时时间。后续文件中如果再使用到socket，不必再设置
+        socket.setdefaulttimeout(timeout) 
+        self.downloadSuccess = False
+        self.index_page = 'index.html'
 
     def getContent(self, url):
-        response = urllib2.urlopen('http://webapplayers.com/inspinia_admin-v1.5/')
-        return response.read().decode('utf-8')
+        self.base_url = url
+		
+        # TODO:如果根路径就是首页则下载
+        urllib.urlretrieve(url, self.save_path + self.index_page, self.download_callbackfunc)
+
+        response = urllib2.urlopen(url)
+        content = response.read().decode('utf-8')
+        response.close()
+        return content
 
     def getUrls(self, content):
         soup = BeautifulSoup(''.join(content))
         result = []
+
+         #a
+        links = soup.findAll('a')
+        for link in links:
+            try:
+                temp = link['src']
+                result.append(temp)
+            except KeyError:
+                pass # or some other fallback action
+
+        #css
+        links = soup.findAll('link')
+        for link in links:
+            try:
+                temp = link['href']
+                result.append(temp)
+            except KeyError:
+                pass # or some other fallback action
+
+        #js
+        links = soup.findAll('script')
+        for link in links:
+            try:
+                temp = link['src']
+                result.append(temp)
+            except KeyError:
+                pass # or some other fallback action
 
         #images
         links = soup.findAll('img')
@@ -37,6 +77,19 @@ class Spider:
     def getdirc(self, url):
         idx = url.rfind('/')
         return url[:idx]
+
+    def download_callbackfunc(self,blocknum, blocksize, totalsize):
+        '''回调函数
+        @blocknum: 已经下载的数据块
+        @blocksize: 数据块的大小
+        @totalsize: 远程文件的大小
+        '''
+        percent = 100.0 * blocknum * blocksize / totalsize
+        if percent > 100:
+            percent = 100
+            self.downloadSuccess = True
+
+        print "%.2f%%"% percent
 
     # 下载到本地
     def download(self, url, save_path):
@@ -54,8 +107,21 @@ class Spider:
             print 'create directory:' + dirs
 
         if not os.path.exists(save_path + url):
-            urllib.urlretrieve(absolute_url, save_path + url)
-            print 'download file:' + absolute_url
+            sleep_time = 1
+            while True:
+                try: # 外包一层try
+                    time.sleep(sleep_time)
+                    urllib.urlretrieve(absolute_url, save_path + url, self.download_callbackfunc)
+                    print 'download file:' + absolute_url
+                   # 只有正常执行方能退出。
+                    if self.downloadSuccess:
+                        break
+                except : # 如果发生了10054或者IOError或者XXXError
+                    sleep_time += 5 #多睡5秒，重新执行以上的download.因为做了检查点的缘故，上面的程序会从抛出异常的地方继续执行。防止了因为网络连接不稳定带来的程序中断。
+                    print('enlarge sleep time:',sleep_time)
+
+
+
 
     # 递归创建文件夹
     def mkdir_p(self, path):
@@ -79,8 +145,10 @@ class Spider:
 
 if __name__ == "__main__":
     spider = Spider()
-    content = spider.getContent('http://webapplayers.com/inspinia_admin-v1.5/');
+    content = spider.getContent('http://webapplayers.com/inspinia_admin-v1.6/Angular/#/dashboards/dashboard_1');
+    spider.save_path = "test/"
     for href in {}.fromkeys(spider.getUrls(content)).keys():
-        spider.download(href, 'temp/')
+        if not href.__contains__('https://maps.googleapis.com/maps/api'):
+            spider.download(href, spider.save_path)
 
 
